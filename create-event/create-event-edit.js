a03f70826dbaf652c9fc728c67b7c09aeb7e1a22
saadghazal <saad.m.ghazal@icloud.com>
2024-06-08 15:05:41 +0300

    go to advanced create

---
+++ create-event/create-event-edit.js
@@ -0,0 +1,46 @@
+saveBtn = document.getElementById("save-btn");
+
+saveBtn.addEventListener('click',saveData);
+
+function saveData(){
+    let confTitle = document.getElementById('conf-title-lb').value;
+    let confShort = document.getElementById('conf-short-lb').value;
+    let startDate = document.getElementById('start-date').value;
+    let endDate = document.getElementById('end-date').value;
+    let country = document.getElementById('location').value;
+    let description = document.getElementById('description').value;
+
+   if(confTitle == ""){
+    window.alert("Please Enter Conference Title")
+    return;
+   }
+   if(confShort == ""){
+    window.alert("Please Enter Conference Short Name")
+    return;
+   }
+   if(startDate == ""){
+    window.alert("Please Enter Start Date")
+    return;
+   }
+   if(endDate == ""){
+    window.alert("Please Enter End Date")
+    return;
+   }
+   if(country == ""){
+    window.alert("Please Enter Location")
+    return;
+   }
+   if(description == ""){
+    window.alert("Please Enter Conference Description")
+    return;
+   }
+   window.localStorage.setItem('create-title',confTitle);
+   window.localStorage.setItem('create-short',confShort);
+   window.localStorage.setItem('create-start-date',startDate);
+   window.localStorage.setItem('create-end-date',endDate);
+   window.localStorage.setItem('create-location',country);
+   window.localStorage.setItem('create-description',description);
+
+   window.location.href = "../create-event/create-event-advanced.html";
+
+}
