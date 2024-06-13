6f168c4e4581ce2e093294ee968aa333d3a2b246
mustafa <mustafahmusameh@gmail.com>
2024-06-13 15:46:40 +0300

    manager accept and reject and delete submission and add and delete member

--- manager/manager.js
+++ manager/manager.js
@@ -7,7 +7,89 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     loadData(token)
     fetchSubmesions();
+
+        const titleInput = document.getElementById('max-num-of-submissions');
+        const emailInput = document.getElementById('max-num-of-attendees');
+        const roleInputs = document.getElementsByName('inlineRadioOptions');
+        const addButton = document.querySelector('.btn-success');
+        const deleteButton = document.querySelector('.btn-danger');
+    
+
+        addButton.addEventListener('click', async () => {
+            const conferenceTitle = titleInput.value;
+            const memberEmail = emailInput.value;
+            alert(memberEmail)
+            let memberRole = '';
+    
+            roleInputs.forEach(input => {
+                if (input.checked) {
+                    memberRole = input.nextElementSibling.textContent.trim();
+                }
+            });
+            alert(memberRole)
+            try {
+                const response = await fetch('http://localhost:3000/manager/add-member', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({conferenceId: localStorage.getItem("conId"), title: conferenceTitle, email:memberEmail, role:memberRole})
+                });
+                window.location.href = "../manager/manager.html";
+            } catch (error) {
+                console.error('Error fetching conferences:', error);
+            }
+    
+        });
+    
+        // Add event listener to the Delete button
+        deleteButton.addEventListener('click', async () => {
+            const memberEmail = emailInput.value;
+            try {
+                const response = await fetch('http://localhost:3000/user/get-conmem', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({ email: memberEmail, conferenceId: localStorage.getItem("conId") })
+                });
+                const conmem = await response.json();
+                if (!response.ok) {
+                    throw new Error(conmem.message || 'Error fetching conference member');
+                }
+    
+                deletemember(conmem);
+            } catch (error) {
+                console.error('Error fetching conference member:', error);
+                alert(`Error fetching conference member: ${error.message}`);
+            }
+        });
     });
+    async function deletemember(conmem){
+        try {
+            alert(conmem._id)
+            const response2 = await fetch('http://localhost:3000/manager/delete-member', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({ conferenceMemberId: conmem._id, conferenceId: localStorage.getItem("conId") })
+            });
+            const result = await response2.json();
+            if (!response2.ok) {
+                throw new Error(result.message || 'Error deleting conference member');
+            }
+            console.log('Member deleted successfully:', result);
+            window.location.href = "../manager/manager.html";
+        } catch (error) {
+            console.error('Error deleting conference member:', error);
+            alert(`Error deleting member: ${error.message}`);
+        }
+
+    }
     async function loadData(token) {
         try {
             const response = await fetch('http://localhost:3000/user/get-user', {
@@ -77,23 +159,91 @@ document.addEventListener('DOMContentLoaded', (event) => {
     async function displayConferences(submissions) {
         const submissionsContainer = document.getElementById("sub");
         submissionsContainer.innerHTML = ''; // Clear existing content
-        
+    
         for (const submission of submissions) {
-            const user = await getuser(submission.authorId)
-            const conferenceHTML = `<div class="col-md-12 col-lg-6 mx-auto gap-5">
-                        <div class="card test-card" style="min-width: 18rem;">
-                            <img src="../assets/conference banner2.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text">Author's name: ${user.fullName}</p>
-                                <p class="card-text">Average score: ${submission.avgScore}</p>
-                            </div>
-                            <div class="d-flex mx-auto gap-2">
-                                <button type="button" class="btn btn-success test-button" data-id="${submission._id}" data-action="accept">Accept</button>
-                                <button type="button" class="btn btn-danger test-button" data-id="${submission._id}" data-action="reject">Reject</button>
-                            </div>
+            const user = await getuser(submission.authorId);
+            let conferenceHTML;
+    
+            if (submission.isAccepted) {
+                conferenceHTML = `<div class="col-md-12 col-lg-6 mx-auto gap-5">
+                    <div class="card test-card" style="min-width: 18rem;">
+                        <img src="../assets/conference banner2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Author's name: ${user.fullName}</p>
+                            <p class="card-text">Average score: ${submission.avgScore}</p>
+                            <p class="card-text">The submission is: Accepted</p>
                         </div>
-                    </div>`;
+                        <div class="d-flex mx-auto gap-2">
+                            <button type="button" class="btn btn-danger test-button" data-id="${submission._id}" data-action="delete">Delete</button>
+                        </div>
+                    </div>
+                </div>`;
+            } else {
+                conferenceHTML = `<div class="col-md-12 col-lg-6 mx-auto gap-5">
+                    <div class="card test-card" style="min-width: 18rem;">
+                        <img src="../assets/conference banner2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Author's name: ${user.fullName}</p>
+                            <p class="card-text">Average score: ${submission.avgScore}</p>
+                            <p class="card-text">The submission is: still need evaluate</p>
+                        </div>
+                        <div class="d-flex mx-auto gap-2">
+                            <button type="button" class="btn btn-success test-button" data-id="${submission._id}" data-action="accept">Accept</button>
+                            <button type="button" class="btn btn-danger test-button" data-id="${submission._id}" data-action="reject">Reject</button>
+                        </div>
+                    </div>
+                </div>`;
+            }
             
             submissionsContainer.insertAdjacentHTML('beforeend', conferenceHTML);
         }
+    
+        // Add event listeners to the buttons
+        const buttons = document.querySelectorAll('.test-button');
+        buttons.forEach(button => {
+            button.addEventListener('click', function(event) {
+                const submissionId = this.getAttribute('data-id');
+                const action = this.getAttribute('data-action');
+                handleSubmissionAction(submissionId, action);
+            });
+        });
     }
+    
+    // Function to handle the button actions
+    async function handleSubmissionAction(submissionId, action) {
+        if (action === 'accept') {
+            const response = await fetch('http://localhost:3000/manager/accept-sub', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({submissionId})
+            });
+            alert(`Accepted submission with ID: ${submissionId}`);
+            window.location.href = "../manager/manager.html";
+        } else if (action === 'reject') {
+            const response = await fetch('http://localhost:3000/manager/reject-sub', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({submissionId})
+            });
+            alert(`Rejected submission with ID: ${submissionId}`);
+            window.location.href = "../manager/manager.html";
+        } else if (action === 'delete') {
+            const response = await fetch('http://localhost:3000/manager/reject-sub', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({submissionId})
+            });
+            alert(`Deleted submission with ID: ${submissionId}`);
+            window.location.href = "../manager/manager.html";
+        }
+    }
+    
